bd90c600683f8299fd6faf3eed142f68986f0c95
Shashi Raj <shashiraj.972@gmail.com>
2022-10-06 20:35:27 +0530

    update the heroku postgresql

--- src/main/java/constants/IDatabase.java
+++ src/main/java/constants/IDatabase.java
@@ -2,9 +2,9 @@ package constants;
 
 public class IDatabase {
 
-	public static final String DRIVER_NAME = "com.mysql.jdbc.Driver";
-	public static final String CONNECTION_STRING = "jdbc:mysql://localhost:3306/onlinebookstore";
-	public static final String USER_NAME = "root";
-	public static final String PASSWORD = "root";
+	public static final String DRIVER_NAME = "org.postgresql.Driver";
+	public static final String CONNECTION_STRING = "jdbc:postgresql://ec2-54-227-248-71.compute-1.amazonaws.com:5432/d1hg5av2eonfhg";
+	public static final String USER_NAME = "leyrjpvzqznbxk";
+	public static final String PASSWORD = "a83479fb7a6879f06d62ac4226070bc18f8f09c4f6fcc68334bf4cf70adb0947";
 
 }
