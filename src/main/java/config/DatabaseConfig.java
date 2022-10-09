8107ba9b15395dc09e0b1ef637befe98e381ebe6
Aman Agarwal <cyclopaman@gmail.com>
2022-10-10 01:13:49 +0530

    moving the hardcioded DB constants to environment variables

--- src/main/java/config/DatabaseConfig.java
+++ src/main/java/config/DatabaseConfig.java
@@ -3,11 +3,11 @@ package config;
 public class DatabaseConfig {
 
 	public static final String DRIVER_NAME ="com.mysql.jdbc.Driver";
-	public static final String DB_HOST = "jdbc:mysql://localhost";
-	public static final String DB_PORT = "3306";
-	public static final String DB_NAME = "onlinebookstore";
-	public static final String DB_USER_NAME = "root";
-	public static final String DB_PASSWORD = "root";
+	public static final String DB_HOST = System.getenv("db_url");
+	public static final String DB_PORT = System.getenv("port");
+	public static final String DB_NAME = System.getenv("db_name");
+	public static final String DB_USER_NAME = System.getenv("db_username");
+	public static final String DB_PASSWORD = System.getenv("db_password");
 	
 	public static final String CONNECTION_STRING = DB_HOST + ":" + DB_PORT + "/" + DB_NAME;
 
