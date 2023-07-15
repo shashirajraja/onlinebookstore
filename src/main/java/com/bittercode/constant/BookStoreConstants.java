305de626c3dcbb8be86d80886359fa3bba5d2484
NIKHIL <vishad0198@gmail.com>
2023-07-15 14:40:54 +0530

    constant added

--- src/main/java/com/bittercode/constant/BookStoreConstants.java
+++ src/main/java/com/bittercode/constant/BookStoreConstants.java
@@ -2,6 +2,6 @@ package com.bittercode.constant;
 
 public interface BookStoreConstants {
 	public static String CONTENT_TYPE_TEXT_HTML = "text/html";
-	
+	public static String CONTENT_TYPE = "html";
 	
 }
