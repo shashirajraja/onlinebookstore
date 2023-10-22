17090458ad86d5351681ceee8ef27e4d4835ed1b
Shashi Raj <34605595+shashirajraja@users.noreply.github.com>
2023-10-22 21:47:43 +0530

    about servlet documentation

--- src/main/java/servlets/AboutServlet.java
+++ src/main/java/servlets/AboutServlet.java
@@ -11,12 +11,13 @@ import javax.servlet.http.HttpServletResponse;
 
 import com.bittercode.model.UserRole;
 import com.bittercode.util.StoreUtil;
-
+//Http Servlet extended class for showing the about information
 public class AboutServlet extends HttpServlet {
 
     public void service(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
         PrintWriter pw = res.getWriter();
         res.setContentType("text/html");
+        //If the store is logged in as customer or seller show about info
         if (StoreUtil.isLoggedIn(UserRole.CUSTOMER, req.getSession())) {
             RequestDispatcher rd = req.getRequestDispatcher("CustomerHome.html");
             rd.include(req, res);
@@ -32,6 +33,8 @@ public class AboutServlet extends HttpServlet {
                     + "        title=\"My Personal Website\" width=\"100%\" height=\"100%\"></iframe>");
 
         } else {
+            //If the user is not logged in, ask to login first
+            //Proceed only if logged in or forword to login page
             RequestDispatcher rd = req.getRequestDispatcher("login.html");
             rd.include(req, res);
             pw.println("<table class=\"tab\"><tr><td>Please Login First to Continue!!</td></tr></table>");
